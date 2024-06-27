c6bf5e5b2f142eb13eb186912843ebec721da17b
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-06-27 13:23:37 -0500

    Update and rename projects-list.js to projects.js

--- js/projects.js
+++ js/projects.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Function to fetch the game list from the external HTML file
     function fetchGameList() {
-        return fetch('list.html')
+        return fetch('https://assets99.netlify.app/minecraftgoldfish3/list.html')
             .then(response => response.text())
             .catch(error => {
                 console.error('Error fetching game list:', error);
